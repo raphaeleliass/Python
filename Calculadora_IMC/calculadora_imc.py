d219416d241ecda8db646ff214a127fa2beed26c
Raphael Elias <raphaeleliass@outlook.com>
2023-10-07 13:06:15 -0300

    Update de correção do código

--- Calculadora_IMC/calculadora_imc.py
+++ Calculadora_IMC/calculadora_imc.py
@@ -2,23 +2,20 @@ from time import sleep
 
 # Apresentação da calculadora
 sleep(1)
-print('Bem-vindo á Calculadora de IMC! Para iniciarmos preciso de algumas informações, vamos começar?')
+print('Bem-vindo à Calculadora de IMC! Para iniciarmos, preciso de algumas informações. Vamos começar?')
 
 sleep(1)
 input('Aperte qualquer tecla para começar')
 
-dnv = 'n'
-while dnv == 's':
+while True:
     # Requisição dos dados do usuário para o cálculo
     nome = input('Digite seu nome: ').strip().title()
-    peso = float(input(F'Olá {nome}, agora preciso que você me diga seu peso: '))
+    peso = float(input(f'Olá {nome}, agora preciso que você me diga seu peso: '))
     altura_cm = int(input('Agora preciso que digite sua altura em centímetros: '))
-
 
     # Conversão da altura em centímetros e cálculo do IMC
     altura = altura_cm / 100.0
     imc = peso / (altura**2)
-
 
     # Classificação do IMC
     def classificacao_imc(imc):
@@ -34,16 +31,14 @@ while dnv == 's':
             return ('obesidade grau II')
         else:
             return ('obesidade mórbida')
-        
+
     # Resultado do usuário
     resultado = classificacao_imc(imc)
-    print(f'{nome}, seu imc é de {imc:.1f} e é considerado como {resultado}')
-    
-    # Função de calcular novamente:
-    while dnv != 's':
-        dnv = input("Deseja fazer outro cálculo? (s/n): ").lower().strip()
-        print('Digite uma opção válida.')
-        if dnv == 's':
-            break
-    break
+    print(f'{nome}, seu IMC é de {imc:.1f} e é considerado como {resultado}')
+
+    # Pergunta se o usuário deseja fazer outro cálculo
+    dnv = input("Deseja fazer outro cálculo? (s/n): ").lower().strip()
+    if dnv != 's':
+        break
+
 print(f'Foi um prazer te ajudar, {nome}. Até logo!')
